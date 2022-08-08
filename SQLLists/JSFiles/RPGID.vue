7da66dfb450f7bc95b7a174110a9e72ba3669cb1
Inkflow <101215221+Inkflow59@users.noreply.github.com>
2022-08-08 10:08:59 +0200

    Add files via upload

---
+++ SQLLists/JSFiles/RPGID.vue
@@ -0,0 +1,20 @@
+<template>
+<head>
+    <title>RPG IDs</title>
+</head>
+</template>
+
+<script>
+export default {
+    name="RPGID"
+    data() {
+        return {
+        fantasy=1
+        modern=2
+        military=3
+        futuristic=4
+        medieval=5
+        }
+    }
+    methods: {}
+}</script>
